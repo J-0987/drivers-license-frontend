e175788a329806f5f1c2abd9401212f3939f697b
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-11-12 11:24:49 -0500

    form styling fix

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ApplicationForm from './components/applicationForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Our Application</h1>
+      <ApplicationForm />
+    </div>
+  );
+}
+
+export default App;
